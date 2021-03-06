9470e610556d722cec8dc1158e3c6733912b3a1a
João Mangueira <jamjs7@gmail.com>
2021-03-06 17:46:00 -0300

    acertando erro

--- src/config/multer.js
+++ src/config/multer.js
@@ -21,17 +21,17 @@ module.exports = {
         },
     }),
     limits: {
-        fileSize: 2 * 1024 + 1024,
+        fileSize: 2 * 1024 * 1024,
     },
     fileFilter: (req, file, cb) => {
-        const allowedMines = [
+        const allowedMimes = [
             'image/jpeg',
             'image/pjpeg',
             'image/png',
             'image/gif'
         ];
 
-        if (allowedMines.includes()) {
+        if (allowedMimes.includes(file.mimetype)) {
             cb(null, true);
         } else {
             cb(new Error('Invalid file type.'));
