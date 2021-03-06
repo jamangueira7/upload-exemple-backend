eabaa8836338cb77c803c48bc12f26315340bf57
João Mangueira <jamjs7@gmail.com>
2021-03-06 18:31:17 -0300

    gravando no mongo

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,22 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
+const Post = require('./models/Post');
 
-routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
-    return res.json({ hello: "Hello Workd"});
+
+routes.post("/posts", multer(multerConfig).single('file'), async (req, res) => {
+    const {
+        originalname: name,
+        size,
+        filename: key
+    } = req.file
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: '',
+    });
+    return res.json(post);
 });
 
 
