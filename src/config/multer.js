2eaa6664ed693db105dde8962bdc389255c6f04e
João Mangueira <jamjs7@gmail.com>
2021-03-06 17:36:36 -0300

    configurações do multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,5 +3,25 @@ const path = require('path');
 const crypto = require('crypto');
 
 module.exports = {
-    des: path.resolve()
-}
+    des: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storege: multer.diskStorage({
+
+    }),
+    limits: {
+        fileSize: 2 * 1024 + 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedMines = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif'
+        ];
+
+        if (allowedMines.includes()) {
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid file type.'));
+        }
+    },
+};
